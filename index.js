4dd2a46975880d6edfd391ecda850eea6c627530
Maxim <334890@gmail.com>
2022-12-26 15:12:55 +1000

    Add grid size changing ability

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
 const root = document.documentElement;
 const screenSize = 32;
 const screen = document.querySelector("#screen");
-const screenSizeSquared = screenSize * screenSize;
 const clearBtn = document.querySelector("#clear");
+const sizeBtn = document.querySelector("#size");
 
 clearBtn.addEventListener("click", clear);
-root.style.setProperty("--screen-size", screenSize);
+sizeBtn.addEventListener("click", size);
 
-for (let i = 1; i <= screenSizeSquared; i++) {
-  const pixel = document.createElement("div");
-  pixel.addEventListener("mouseover", draw);
-  pixel.classList.add("pixel");
-  screen.appendChild(pixel);
+function createPixels(screenSize) {
+  const screenSizeSquared = screenSize * screenSize;
+  screen.replaceChildren();
+  root.style.setProperty("--screen-size", screenSize);
+  for (let i = 1; i <= screenSizeSquared; i++) {
+    const pixel = document.createElement("div");
+    pixel.addEventListener("mouseover", draw);
+    pixel.classList.add("pixel");
+    screen.appendChild(pixel);
+  }
 }
+
+createPixels(screenSize);
 
 function draw(event) {
   event.target.classList.add("darker");
@@ -21,4 +28,12 @@ function draw(event) {
 function clear() {
   const pixels = document.querySelectorAll(".pixel");
   pixels.forEach((pixel) => pixel.classList.remove("darker"));
+}
+
+function size() {
+  let newSize = 0;
+  do {
+    newSize = +prompt("Choose a grid size 10 to 100 pixels");
+  } while (newSize < 10 || newSize > 100);
+  createPixels(newSize);
 }
