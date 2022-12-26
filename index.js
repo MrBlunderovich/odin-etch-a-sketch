44543624926826c0d3f133939a4a35b2c890e51a
Maxim <334890@gmail.com>
2022-12-26 14:29:31 +1000

    Add drawing ability

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const root = document.documentElement;
-const screenSize = 16;
+const screenSize = 32;
+const screen = document.querySelector("#screen");
+const screenSizeSquared = screenSize * screenSize;
+
 root.style.setProperty("--screen-size", screenSize);
 
-const screen = document.querySelector("#screen");
-
-const screenSizeSquared = screenSize * screenSize;
 for (let i = 1; i <= screenSizeSquared; i++) {
-  let pixel = document.createElement("div");
+  const pixel = document.createElement("div");
+  pixel.addEventListener("mouseover", draw);
   pixel.classList.add("pixel");
   screen.appendChild(pixel);
+}
+
+function draw(event) {
+  event.target.classList.add("darker");
 }
